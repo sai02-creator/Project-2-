5e1146c38ee64c2d7a3e136ffd8f0cb651dfd59c
sai02-creator <saichaitanyagaddam1@gmail.com>
2024-08-08 00:34:28 +1000

    stage all changes

---
+++ questions.js
@@ -0,0 +1,57 @@
+ let questions = [
+    {
+numb: 1,
+questions:"what does HTML stand for?",
+answers:"c. Hyper Text Markup Language",
+options:[
+    "A. Hyper Text Multi Language",
+    "B. Hyper Text Multiple Language",
+    "C. Hyper Text Markup Language",
+    "D. Home Text Multi Language "
+
+]
+    },
+    {
+        numb: 2,
+        questions:"What does CSS stand for?",
+        answers:"A. Cascading Style Sheet",
+        options:[
+        "A. Cascading Style Sheet",
+        "B. Cute Style Sheet",
+        "C. Computer Style Sheet",
+        "D. Codehal Style Sheet"
+        ]
+            },{
+                numb: 3,
+                questions:"What does PHP stand for?",
+                answers:"A. Hypertext Preprocessor",
+                options:[
+                    "A. Hypertext Preprocessor",
+                    "B. Hometext Programming",
+                    "C. Hypertext Preprogrammig",
+                    "D. Programming Hypertext Preprocessor"
+                
+                ]
+                    },{
+                        numb: 4,
+                        questions:"What does SQL stand for?",
+                        answers:"D. Structured Query Language",
+                        options:[
+                        "A. Strength Query Language",
+                        "B. Stylesheet Query Language",
+                        "C. Science Question Language",
+                        "D. Structured Query Language"
+                        ]
+                            },{
+                                numb: 5,
+                                questions:"What does XML stand for?",
+                                answers:"D. Extensible Markup Language",
+                                options:[
+                                    "A. Excellent Multiple Language",
+                                    "B. Explorre Multiple Language",
+                                    "C. Extra Markup Language",
+                                    "D. Extensible Markup Language"
+                                
+                                ]
+                            }
+ ];
